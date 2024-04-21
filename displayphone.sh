64772ad58d3fa6009d0bc4eac4df005d4cd3188d
loliteToT <unmasked8578@gmail.com>
2024-04-21 19:46:15 -0300

    handle errors

--- displayphone.sh
+++ displayphone.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 ADB_COMMAND_START="adb shell wm size 1440x2560"
 ADB_COMMAND_END="adb shell wm size 1080x2400"
+
+check_error() {
+    if [ $? -ne 0 ]; then
+        echo "An error has occurred"
+        read -s -r -p "Press any key to exit..."
+        exit 1
+    fi
+}
+
 echo "Setting screen resolution and starting scrcpy..."
 $ADB_COMMAND_START
+check_error
+
 scrcpy -b 30m &
 scrcpy_pid=$!
 wait $scrcpy_pid
 echo "scrcpy closed, restoring phone resolution..."
+check_error
+
+
 $ADB_COMMAND_END
+check_error
+wait .5 #wait half a second before continuing
